38ec9b9557c3c614f9750e71987bf8a6f08eb9e0
Dmitrii Nikulin <nikulas.nikulin@yandex.ru>
2026-07-06 22:32:03 +0300

    services

---
+++ services/telephony/app/db/db_models.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from enum import Enum
+from sqlmodel import SQLModel, Field
+import json
+
+
+
+class CallState(str, Enum):
+    APPEARED="appeared"
+    CONNECTED="connected"
+    ON_HOLD="on hold"
+    DISCONNECTED="disconnected"
+
+class RecordingState(str, Enum):
+    STARTED="started"
+    CONTINUED="continued"
+    COMPLETED="completed"
+
+class BaseCall(SQLModel):
+    id: str
+    start_time: datetime
+    end_time: datetime
+
+class Calls(BaseCall, table=True):
+    __tablename__="calls"
+
+    client_phone_hash: str
+    client_phone_masked: str
+    line_number: str
+    call_state: str
+
+class CallRecording(BaseCall):
+    id: str
+    recording_state: str
+    completion_code: str
+    timestamp: datetime
+
+class Transcripts(SQLModel):
+    id: int
+    call_id: str
+    content: json
+    recieved_at: datetime
